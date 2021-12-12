608c3ffe7591a0d90d2ea395be3a9452492b9134
talgat55 <lightx_design@yahoo.com>
2021-12-12 23:49:00 +0600

    add in header profile and menu profile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   public path: any;
-  constructor() { }
+  public activeClassHeaderProfileMenu: boolean = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+  handleClickOnSideMenuHeader(event: Event) {
+    this.activeClassHeaderProfileMenu = !this.activeClassHeaderProfileMenu;//
+  }
+
 
 }
